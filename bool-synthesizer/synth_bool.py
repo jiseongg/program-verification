922f69ad13ef91ad402978a780be640ef2876c7a
jiseongg <jiseongg301@gmail.com>
2021-01-18 14:42:10 +0900

    <feat>: complete demo version of boolean synthesizer
    TODO: design logic to effectively search unknown parameter, the number of terms

--- bool-synthesizer/synth_bool.py
+++ bool-synthesizer/synth_bool.py
@@ -5,11 +5,53 @@ from z3 import *
 
 
 def encode_logics(P, Q, Z, pos_inputs, neg_inputs):
-    raise NotImplementedError
+    term_cnt = len(P)
+
+    # one of z_ik for kth positive input must be true
+    # if z_ik is true
+    # 1) ith term cannot contain !x_j, if x_j is True
+    # 1) ith term cannot contain x_j, if x_j is False
+    positive_constraints = []
+    for k, pos_input in enumerate(pos_inputs):
+        positive_constraints.append(Or([Z[i][k] for i in range(term_cnt)]))
+        for i in range(term_cnt):
+            for j, x_j in enumerate(pos_input):
+                if x_j == '1':
+                    positive_constraints.append(
+                            Or(Not(Z[i][k]), Not(Q[i][j])))
+                else:
+                    positive_constraints.append(
+                            Or(Not(Z[i][k]), Not(P[i][j])))
+
+    # for negative input, one of follows are True
+    # 1) ith term contain !x_j where x_j is True
+    # 2) ith term doesn't contain x_j where x_j is False
+    negative_constraints = []
+    for k, neg_input in enumerate(neg_inputs):
+        for i in range(term_cnt):
+            negative_constraints.append(
+                    Or([Q[i][j] if x_j == '1'
+                        else P[i][j]
+                        for j, x_j in enumerate(neg_input)]))
+
+    return positive_constraints + negative_constraints
 
 
-def decode_logics(P_sols, Q_sols, Z_sols):
-    raise NotImplementedError
+def decode_logics(P_sols, Q_sols):
+    terms = []
+    for i, (p_i, q_i) in enumerate(zip(P_sols, Q_sols)):
+        term_i_elements = []
+        for j, (p_ij, q_ij) in enumerate(zip(p_i, q_i)):
+            if p_ij:
+                term_i_elements.append('x%d' % (j + 1))
+            if q_ij:
+                term_i_elements.append('!x%d' % (j + 1))
+        term_i_str = ' /\\ '.join(term_i_elements)
+        terms.append('(' + term_i_str + ')')
+
+    formula_str = ' \\/ '.join(terms)
+
+    return formula_str
 
 
 def main():
@@ -33,8 +75,8 @@ def main():
                 for i in range(term_cnt)]
         Q = [[Bool('q_%d%d' % (i, j)) for j in range(num_args)]
                 for i in range(term_cnt)]
-        Z = [[Bool('z_%d%d' % (i, j)) for j in range(term_cnt)]
-                for i in range(num_pos)]
+        Z = [[Bool('z_%d%d' % (i, j)) for j in range(num_pos)]
+                for i in range(term_cnt)]
         F = encode_logics(P, Q, Z, pos_inputs, neg_inputs)
 
         s = Solver()
@@ -47,11 +89,11 @@ def main():
             Q_sols = [[m.evaluate(Q[i][j]) for j in range(num_args)]
                     for i in range(term_cnt)]
             Z_sols = [[m.evaluate(Z[i][j]) for j in range(num_pos)]
-                    for j in range(term_cnt)]
-            answer = decode_logics(P_sols, Q_sols, Z_sols)
+                    for i in range(term_cnt)]
+            answer = decode_logics(P_sols, Q_sols)
             print('Learned function with size = %d' % term_cnt)
             print('f(X) = %s' % answer)
-            break
+            sys.exit(0)
         else:
             continue
 
